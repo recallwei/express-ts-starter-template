2a8a17d7b97821d5cf5b96a3132ee97690d7b963
Bruce Song <recall4056@gmail.com>
2022-07-24 02:34:07 +0800

    Add swagger doc.

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var docsRouter = require("./routes/docs");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const swaggerJsdoc = require("express-jsdoc-swagger");
+const options = {
+  info: {
+    title: "Bruce's Wiki API",
+    version: "1.0.0",
+    description: "An API which is used to support the wiki website.",
+  },
+  security: {
+    bearerAuth: {
+      type: "http",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+      description: "bearer JWT Token",
+      name: "Authorization",
+      in: "header",
+    },
+  },
+  baseDir: __dirname,
+  filesPattern: "./**/*.js",
+  swaggerUIPath: "/api",
+};
+
+swaggerJsdoc(app)(options);
+
+app.use("/", indexRouter);
+app.use("/docs", docsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/docs.js
+++ routes/docs.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/test", function (req, res, next) {
+router.get("/category", function (req, res, next) {
   res.send(["123", "234"]);
 });
 
