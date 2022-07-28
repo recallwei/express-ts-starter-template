a179c824cc277d534d7829957fd356b5705f75d8
Bruce Song <recall4056@gmail.com>
2022-07-29 00:54:04 +0800

    Add prisma query API.

--- src/routes/docs/menus/index.ts
+++ src/routes/docs/menus/index.ts
@@ -1,7 +1,10 @@
 import express, { Router, Request, Response } from "express";
 import { dbPool } from "../../../db/index";
+import { PrismaClient } from "@prisma/client";
 
 const router: Router = express.Router();
+
+const prisma = new PrismaClient();
 
 /**
  * GET /docs/menus
@@ -12,12 +15,23 @@ const router: Router = express.Router();
 router.get("/", async (request: Request, response: Response) => {
   try {
     const { rows } = await dbPool.query(
-      "SELECT * FROM docs.menus ORDER BY indexs"
+      "SELECT * FROM docs.menus ORDER BY index"
     );
     response.status(200).json(rows);
   } catch (err) {
     response.status(404).json("Server Error");
   }
+});
+
+/**
+ * GET /docs/menus/v2
+ * @summary Docs
+ * @tags Docs
+ * @return {object} 200 - OK
+ */
+router.get("/v2", async (request: Request, response: Response) => {
+  const menus = await prisma.menus.findMany();
+  response.status(200).json(menus);
 });
 
 export default router;
