aa5db119d9e158923d3e3aad427631b2ec4e99c6
Bruce Song <recall4056@gmail.com>
2022-07-31 01:10:24 +0800

    Add logging middleware.

--- src/db/index.ts
+++ src/db/index.ts
@@ -21,7 +21,23 @@ interface CustomNodeJSGlobal extends Global {
 
 declare const global: CustomNodeJSGlobal;
 
-const prisma: PrismaClient = global.prisma || new PrismaClient();
+const prisma: PrismaClient =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+    errorFormat: "pretty",
+  });
+
+// Logging middleware
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+  console.log(
+    `Query ${params.model}.${params.action} took ${after - before}ms`
+  );
+  return result;
+});
 
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
--- src/routes/docs/menus/index.ts
+++ src/routes/docs/menus/index.ts
@@ -5,12 +5,12 @@ import { type menus } from "@prisma/client";
 const router: Router = express.Router();
 
 /**
- * GET /docs/menus
+ * GET /docs/menus/v2
  * @summary Docs
  * @tags Docs
  * @return {object} 200 - OK
  */
-router.get("/", async (request: Request, response: Response) => {
+router.get("/v2", async (request: Request, response: Response) => {
   try {
     const { rows } = await dbPool.query(
       "SELECT * FROM docs.menus ORDER BY index"
@@ -22,12 +22,12 @@ router.get("/", async (request: Request, response: Response) => {
 });
 
 /**
- * GET /docs/menus/v2
+ * GET /docs/menus
  * @summary Docs
  * @tags Docs
  * @return {object} 200 - OK
  */
-router.get("/v2", async (request: Request, response: Response) => {
+router.get("/", async (request: Request, response: Response) => {
   const menus: Array<menus> = await prisma.menus.findMany();
   response.status(200).json(menus);
 });
