5bab2325080a359b98ba58a29b1ba1bb34c51d29
Bruce Song <recall4056@gmail.com>
2022-07-31 00:07:45 +0800

    Development env use single instance.

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,6 +15,16 @@ const dbConfig: ClientConfig = {
 
 const dbPool = new Pool(dbConfig);
 
-const prisma: PrismaClient = new PrismaClient();
+interface CustomNodeJSGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+declare const global: CustomNodeJSGlobal;
+
+const prisma: PrismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
+}
 
 export { dbConfig, dbPool, prisma };
