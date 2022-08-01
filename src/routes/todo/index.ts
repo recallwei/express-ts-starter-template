d9f3bbe6ef46a5abe3a3a1435dc7859b988c3816
Bruce Song <recall4056@gmail.com>
2022-08-01 08:49:56 +0800

    Update API.

--- src/routes/todo/index.ts
+++ src/routes/todo/index.ts
@@ -1,6 +1,6 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
 import { prisma } from "dbContext";
-import { type todo } from "@prisma/client";
+import { Prisma, type todo } from "@prisma/client";
 
 const router: Router = express.Router();
 
@@ -12,11 +12,37 @@ const router: Router = express.Router();
  */
 router.get("/", async (request: Request, response: Response) => {
   const todoList: Array<todo> = await prisma.todo.findMany();
+  console.log(todoList);
   response.status(200).json(todoList);
 });
 
-router.post("/", async (request: Request, response: Response) => {
-  response.status(200).send("OK");
-});
+/**
+ * POST /todo
+ * @summary Todo
+ * @tags Todo
+ * @return {object} 200 - success response - application/json
+ */
+router.post(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const todo = await prisma.todo.create({
+        data: {
+          name: "测试测试测试测试测试测测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试试测试测试测试测试测试测试测试测试测试测试测试测试测试测试",
+          url: "",
+          description: "",
+        },
+      });
+      if (todo) {
+        response.status(200);
+      }
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        console.log(e.message);
+        response.status(400).send(e.message);
+      }
+    }
+  }
+);
 
 export default router;
