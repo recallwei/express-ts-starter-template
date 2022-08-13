ec5fd953c36b44a1ed51faafcc15698fb12d1196
Bruce Song <recall4056@gmail.com>
2022-08-13 12:36:04 +0800

    Remove header content-type.

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.all("*", function (req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", " 3.2.1");
-  res.header("Content-Type", "application/json;charset=utf-8");
+  // res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
 
